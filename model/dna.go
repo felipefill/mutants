27f7bc2243916d0833952a4357a65e28fb232264
Felipe Fill Cardoso <felipefill@pm.me>
2019-01-08 15:26:45 -0200

    Moving some more code to DNACheck struct

--- model/dna.go
+++ model/dna.go
@@ -1,23 +1,72 @@
 package model
 
-// DNA represents a DNA
-type DNA struct {
-	DNA []string `json:"Dna"`
-}
+import (
+	"encoding/json"
+	"errors"
+)
 
 const repetitionRequiredForSequence int = 4
 
+// DNACheck represents a DNA check
+type DNACheck struct {
+	DNA []string `json:"Dna"`
+}
+
+// NewDNACheckFromJSONString creates a DNA check from a json string
+func NewDNACheckFromJSONString(data string) (DNACheck, error) {
+	dnaCheck := DNACheck{}
+	err := json.Unmarshal([]byte(data), &dnaCheck)
+
+	if err != nil {
+		return DNACheck{}, errors.New("Could not parse DNA check :" + err.Error())
+	}
+
+	err = dnaCheck.validate()
+	if err != nil {
+		return DNACheck{}, err
+	}
+
+	return dnaCheck, nil
+}
+
+// IsMutant checks whether this is a DNA sequence from a mutant
+func (dnaCheck *DNACheck) IsMutant() bool {
+	count := 0
+
+	for row := 0; row < len(dnaCheck.DNA) && count < 2; row++ {
+		for column := 0; column < len(dnaCheck.DNA[row]) && count < 2; column++ {
+			if dnaCheck.CheckSequenceToTheRight(row, column) {
+				count++
+			}
+
+			if dnaCheck.CheckSequenceDown(row, column) {
+				count++
+			}
+
+			if dnaCheck.CheckSequenceDiagonalLeft(row, column) {
+				count++
+			}
+
+			if dnaCheck.CheckSequenceDiagonalRight(row, column) {
+				count++
+			}
+		}
+	}
+
+	return count > 1
+}
+
 // CheckSequenceToTheRight checks whether there's a repetition match to the right of given position
-func (dna *DNA) CheckSequenceToTheRight(row, column int) bool {
-	if len(dna.DNA[row])-column < repetitionRequiredForSequence {
+func (dnaCheck *DNACheck) CheckSequenceToTheRight(row, column int) bool {
+	if len(dnaCheck.DNA[row])-column < repetitionRequiredForSequence {
 		return false
 	}
 
-	requiredBase := dna.DNA[row][column]
+	requiredBase := dnaCheck.DNA[row][column]
 	c := column + 1
 
 	for loop := 0; loop < repetitionRequiredForSequence-1; loop++ {
-		if dna.DNA[row][c] != requiredBase {
+		if dnaCheck.DNA[row][c] != requiredBase {
 			return false
 		}
 
@@ -28,16 +77,16 @@ func (dna *DNA) CheckSequenceToTheRight(row, column int) bool {
 }
 
 // CheckSequenceDown checks whether there's a repetition match looking down of given position
-func (dna *DNA) CheckSequenceDown(row, column int) bool {
-	if len(dna.DNA)-row < repetitionRequiredForSequence {
+func (dnaCheck *DNACheck) CheckSequenceDown(row, column int) bool {
+	if len(dnaCheck.DNA)-row < repetitionRequiredForSequence {
 		return false
 	}
 
-	requiredBase := dna.DNA[row][column]
+	requiredBase := dnaCheck.DNA[row][column]
 	r := row + 1
 
 	for loop := 0; loop < repetitionRequiredForSequence-1; loop++ {
-		if dna.DNA[r][column] != requiredBase {
+		if dnaCheck.DNA[r][column] != requiredBase {
 			return false
 		}
 
@@ -48,17 +97,17 @@ func (dna *DNA) CheckSequenceDown(row, column int) bool {
 }
 
 // CheckSequenceDiagonalLeft checks whether there's a repetition match in the left diagonal of given position
-func (dna *DNA) CheckSequenceDiagonalLeft(row, column int) bool {
+func (dnaCheck *DNACheck) CheckSequenceDiagonalLeft(row, column int) bool {
 	if row >= repetitionRequiredForSequence || column < repetitionRequiredForSequence-1 {
 		return false
 	}
 
-	requiredBase := dna.DNA[row][column]
+	requiredBase := dnaCheck.DNA[row][column]
 	r := row + 1
 	c := column - 1
 
 	for loop := 0; loop < repetitionRequiredForSequence-1; loop++ {
-		if dna.DNA[r][c] != requiredBase {
+		if dnaCheck.DNA[r][c] != requiredBase {
 			return false
 		}
 
@@ -70,17 +119,17 @@ func (dna *DNA) CheckSequenceDiagonalLeft(row, column int) bool {
 }
 
 // CheckSequenceDiagonalRight checks whether there's a repetition match in the right diagonal of given position
-func (dna *DNA) CheckSequenceDiagonalRight(row, column int) bool {
+func (dnaCheck *DNACheck) CheckSequenceDiagonalRight(row, column int) bool {
 	if row >= repetitionRequiredForSequence || column >= repetitionRequiredForSequence {
 		return false
 	}
 
-	requiredBase := dna.DNA[row][column]
+	requiredBase := dnaCheck.DNA[row][column]
 	r := row + 1
 	c := column + 1
 
 	for loop := 0; loop < repetitionRequiredForSequence-1; loop++ {
-		if dna.DNA[r][c] != requiredBase {
+		if dnaCheck.DNA[r][c] != requiredBase {
 			return false
 		}
 
@@ -89,4 +138,53 @@ func (dna *DNA) CheckSequenceDiagonalRight(row, column int) bool {
 	}
 
 	return true
+}
+
+func (dnaCheck *DNACheck) validate() error {
+	if !dnaCheck.isValidNxNTable() {
+		return errors.New("DNA is not an NxN table")
+	}
+
+	if !dnaCheck.validateDNAHasOnlyValidBases() {
+		return errors.New("DNA has invalid bases")
+	}
+
+	return nil
+}
+
+func (dnaCheck *DNACheck) isValidNxNTable() bool {
+	// This will give the number of rows
+	hypothesis := len(dnaCheck.DNA)
+
+	for row := 0; row < hypothesis; row++ {
+		// If we have a number of columns that's different from our hypothesis
+		if len(dnaCheck.DNA[row]) != hypothesis {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (dnaCheck *DNACheck) validateDNAHasOnlyValidBases() bool {
+	//TODO: This could be done while checking DNA
+	for row := 0; row < len(dnaCheck.DNA); row++ {
+		for column := 0; column < len(dnaCheck.DNA[row]); column++ {
+			currentChar := dnaCheck.DNA[row][column]
+			if !isValidDNABase(currentChar) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func isValidDNABase(c byte) bool {
+	switch c {
+	case 'A', 'T', 'C', 'G':
+		return true
+	default:
+		return false
+	}
 }
--- mutant/main.go
+++ mutant/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/felipefill/mutants/model"
@@ -21,12 +18,12 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		return events.APIGatewayProxyResponse{Body: "", StatusCode: 400}, nil
 	}
 
-	dna, err := validateRequest(request.Body)
+	dnaCheck, err := model.NewDNACheckFromJSONString(request.Body)
 	if err != nil {
 		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
 	}
 
-	if !isMutant(dna.DNA) {
+	if !isMutant(dnaCheck.DNA) {
 		return events.APIGatewayProxyResponse{Body: "", StatusCode: 403}, nil
 	}
 
@@ -34,91 +31,12 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 }
 
 func isMutant(data []string) bool {
-	count := 0
-	dna := model.DNA{DNA: data}
+	// This is being done this way so that the code complies with the requirements
+	// Which is having a function with this signature
+	// My apprach would be to have it related to DNACheck struct
+	dnaCheck := model.DNACheck{DNA: data}
 
-	for row := 0; row < len(dna.DNA) && count < 2; row++ {
-		for column := 0; column < len(dna.DNA[row]) && count < 2; column++ {
-			if dna.CheckSequenceToTheRight(row, column) {
-				count++
-			}
-
-			if dna.CheckSequenceDown(row, column) {
-				count++
-			}
-
-			if dna.CheckSequenceDiagonalLeft(row, column) {
-				count++
-			}
-
-			if dna.CheckSequenceDiagonalRight(row, column) {
-				count++
-			}
-		}
-	}
-
-	return count > 1
-}
-
-func validateRequest(body string) (model.DNA, error) {
-	data, err := parseDNAFromString(body)
-	if err != nil {
-		return model.DNA{}, errors.New("Could not parse request :" + err.Error())
-	}
-
-	if !isValidNxNTable(data.DNA) {
-		return data, errors.New("DNA is not an NxN table")
-	}
-
-	if !validateDNAHasOnlyValidBases(data.DNA) {
-		return data, errors.New("DNA has invalid bases")
-	}
-
-	return data, nil
-}
-
-func isValidNxNTable(dna []string) bool {
-	// This will give the number of rows
-	hypothesis := len(dna)
-
-	for row := 0; row < hypothesis; row++ {
-		// If we have a number of columns that's different from our hypothesis
-		if len(dna[row]) != hypothesis {
-			return false
-		}
-	}
-
-	return true
-}
-
-func parseDNAFromString(str string) (model.DNA, error) {
-	data := model.DNA{}
-	err := json.Unmarshal([]byte(str), &data)
-
-	return data, err
-}
-
-func validateDNAHasOnlyValidBases(dna []string) bool {
-	//TODO: This could be done while checking DNA
-	for row := 0; row < len(dna); row++ {
-		for column := 0; column < len(dna[row]); column++ {
-			currentChar := dna[row][column]
-			if !isValidDNABase(currentChar) {
-				return false
-			}
-		}
-	}
-
-	return true
-}
-
-func isValidDNABase(c byte) bool {
-	switch c {
-	case 'A', 'T', 'C', 'G':
-		return true
-	default:
-		return false
-	}
+	return dnaCheck.IsMutant()
 }
 
 func main() {
